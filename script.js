b98ee89f65d2c89172499296cebd6ac70eb71d80
Ryan <ryan@RyandeMacBook-Pro.local>
2020-01-09 18:40:15 -0500

    added script

---
+++ script.js
@@ -0,0 +1,8 @@
+var myName = 'Chang Li-Han'
+console.log(`Hello, my name is ${myName}`)
+var number = 5
+var step1 = number * 2
+var step2 = step1 * 5
+var step3 = step2 / number
+var step4 = step3 - 7
+console.log(step4)
